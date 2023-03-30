a7a955cb8e5d6a1ecb663669493597bd84de98d6
holisticdev <holisticxsoft@gmail.com>
2023-03-30 13:05:28 -0500

    Added function delete users

--- db.py
+++ db.py
@@ -15,14 +15,16 @@ cursor.execute("""CREATE TABLE IF NOT EXISTS users(
 connection.commit()
 
 
-def create_users(users):
+def create_user(user):
     with connection:
-        for user in users:
+        try:
             cursor.execute(
                 "INSERT INTO users VALUES (:username, :last_access, :inactive_at, :delete_at, :created_at, :updated_at)",
                 {'username': user.username, 'last_access': user.last_access, 'inactive_at': user.inactive_at,
                  'delete_at': user.delete_at, 'created_at': user.created_at,
                  'updated_at': user.updated_at})
+        except:
+            pass
 
 
 def get_users():
@@ -38,3 +40,9 @@ def update_users(users):
                 cursor.execute(f"UPDATE users SET inactive_at='{user.inactive_at}' WHERE username='{user.username}'")
             if user.delete_at != '':
                 cursor.execute(f"UPDATE users SET delete_at='{user.delete_at}' WHERE username='{user.username}'")
+
+
+def get_inactive_users():
+    with connection:
+        cursor.execute("SELECT * FROM users WHERE inactive_at <> ''")
+        return cursor.fetchall()
